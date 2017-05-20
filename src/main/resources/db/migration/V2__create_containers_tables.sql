707211ed1f909892dae31ee34d09f87d0ac38df5
syzhang <syzhang@thoughtworks.com>
2017-05-20 15:43:20 +0800

    use uppercase in migration

--- src/main/resources/db/migration/V2__create_containers_tables.sql
+++ src/main/resources/db/migration/V2__create_containers_tables.sql
@@ -1,19 +1,19 @@
-CREATE TABLE providers (
+CREATE TABLE PROVIDERS (
   id INT AUTO_INCREMENT PRIMARY KEY,
   name VARCHAR(255) NOT NULL
 );
 
-CREATE TABLE jobs (
+CREATE TABLE JOBS (
   id INT AUTO_INCREMENT PRIMARY KEY,
   provider_id INT,
   FOREIGN KEY (provider_id) REFERENCES providers(id)
 );
 
-CREATE TABLE containers (
+CREATE TABLE CONTAINERS (
   id INT PRIMARY KEY
 );
 
-CREATE TABLE jobcontainer (
+CREATE TABLE JOBCONTAINER (
   job_id INT,
   container_id INT,
   FOREIGN KEY (job_id) REFERENCES jobs(id),
